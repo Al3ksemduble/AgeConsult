26bfccad8e44c37f4830e2ce94818736b077c86c
Al3ksemduble <ah097786@gmail.com>
2024-04-22 10:24:25 -0300

    Adicionar arquivos de projeto.

---
+++ age/FrmAge.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace age
+{
+    partial class FrmAge
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblinstrucao = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnVerif = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSair.Location = new System.Drawing.Point(25, 397);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(110, 41);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblinstrucao
+            // 
+            this.lblinstrucao.AutoSize = true;
+            this.lblinstrucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblinstrucao.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblinstrucao.Location = new System.Drawing.Point(78, 94);
+            this.lblinstrucao.Name = "lblinstrucao";
+            this.lblinstrucao.Size = new System.Drawing.Size(110, 17);
+            this.lblinstrucao.TabIndex = 3;
+            this.lblinstrucao.Text = "Digite sua Idade";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResult.Location = new System.Drawing.Point(6, 39);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(99, 25);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Resultado";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitulo.Location = new System.Drawing.Point(282, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(170, 25);
+            this.lblTitulo.TabIndex = 5;
+            this.lblTitulo.Text = "Consulta de idade";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Location = new System.Drawing.Point(25, 278);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 94);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(194, 91);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(246, 20);
+            this.txtIdade.TabIndex = 7;
+            // 
+            // btnClean
+            // 
+            this.btnClean.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClean.Location = new System.Drawing.Point(204, 209);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(110, 41);
+            this.btnClean.TabIndex = 8;
+            this.btnClean.Text = "Limpar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btnVerif
+            // 
+            this.btnVerif.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnVerif.Location = new System.Drawing.Point(330, 209);
+            this.btnVerif.Name = "btnVerif";
+            this.btnVerif.Size = new System.Drawing.Size(110, 41);
+            this.btnVerif.TabIndex = 9;
+            this.btnVerif.Text = "Verificar";
+            this.btnVerif.UseVisualStyleBackColor = true;
+            this.btnVerif.Click += new System.EventHandler(this.btnVerif_Click);
+            // 
+            // FrmAge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.btnVerif);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblinstrucao);
+            this.Controls.Add(this.btnSair);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "FrmAge";
+            this.Text = "Digite sua Idade";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblinstrucao;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnVerif;
+    }
+}
+
---
+++ age/FrmAge.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace age
+{
+    public partial class FrmAge : Form
+    {
+        public FrmAge()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerif_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtIdade.Text, out int idade))
+            {
+                MessageBox.Show("Por favor, digite uma idade válida.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (idade >= 18)
+            {
+                lblResult.Text = "Você é maior de idade!";
+            }
+            else
+            {
+                lblResult.Text = "Você é menor de idade!";
+            }
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtIdade.Clear();
+            txtIdade.Focus();
+        }
+        
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
